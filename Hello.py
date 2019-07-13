ea99cdf365f1f2b53a288d851f3d46c392a9699d
eung33 <eung33@naver.com>
2019-07-13 22:49:49 +0900

    learn coding

---
+++ Hello.py
@@ -0,0 +1,12 @@
+import math
+def f(n):
+    if(n<10) :
+        return n
+    else :
+        return f(n/10)+(n%10)*pow(10.0,round(math.log(n,10)))
+
+a=f(3423)
+b=math.log(3423,10)
+print(a)
+print(round(b))
+
---
+++ learnbash.sh
@@ -0,0 +1,10 @@
+
+true  # 쉘 내장명령어인 "true".
+echo "\"true\"의 종료 상태 = $?"     # 0
+
+! true
+echo "\"! true\"의 종료 상태 = $?"   # 1
+# 조심할 점은 "!"을 쓸 때, 빈 칸이 있어야 된다는 것입니다.
+# 그냥 !true 라고 쓰면 "command not found" 에러가 납니다.
+
+# Thanks, S.C.
