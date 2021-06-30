6d6cd2d30d02692241859f42130d7a5e58f5106b
EG Kim <eung33@naver.com>
2021-06-30 23:33:54 +0900

    shutdown all command created

---
+++ GongbroDesk/exercise.py
@@ -0,0 +1,13 @@
+vip_names = ['Newton', 'Euler', 'Gauss']
+print(vip_names)
+
+alphabet = ['b', 'a', 'c', 'd', 'q']
+print(alphabet)
+alphabet.sort()
+print(alphabet)  
+
+
+
+import math
+
+math.floor(math.pi)
